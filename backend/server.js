4b6982537c8599b41987f87ce0c42ebd9cfa2081
Psychopomp3012 <diwakar.sachin3012@gmail.com>
2024-08-19 20:54:16 +0530

    1.2nd commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios'); // to handle http requests
 
 const app = express()
 
@@ -13,16 +14,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    // http://ip-api.com/json/24.48.0.1
+    const ip = req.headers['cf-connecting-ip'] || req.socket.remoteAddress || req.headers['x-real-ip'] || req.headers['x-forwarded-for']
+    const response = await axios.get(`http://ip-api.com/json/${ip}`);
     res.send(`
         <h3>1: ${req.socket.remoteAddress}</h3>
         <h3>2: ${req.headers['x-real-ip']}</h3>
         <h3>3: ${req.headers['x-forwarded-for']}</h3>
         <h3>4(cloudfare): ${req.headers['cf-connecting-ip']}</h3>
         <h3>Public IP=> ${req.ip}</h3>
+
+        <h1>Details: </h1>
+        <h3>${JSON.stringify(response.data)}</h3>
     `);
-    
 });
+
 
 app.listen(port, () => {
     console.log(`running on ${port}`);
